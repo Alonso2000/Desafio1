8590ca5ebb76a70222d3457dbf522198a1c6c156
Alonso <alonsete2000@gmail.com>
2022-09-27 12:52:20 +0200

    Añado el metodo toString() a clase orden

--- src/main/kotlin/Orden.kt
+++ src/main/kotlin/Orden.kt
@@ -1,9 +1,14 @@
-import java.time.DayOfWeek
 import java.time.LocalDate
-import java.util.Date
 
 class Orden {
     var fecha: LocalDate? = LocalDate.now()
     var completado:Boolean = false
     var destino = Array(3){Array<Any>(3){0} }
+
+
+    override fun toString(): String {
+        return "Orden(fecha=$fecha, completado=$completado, destino=${destino.contentToString()})"
+    }
+
+
 }
