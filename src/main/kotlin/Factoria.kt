1ffbf63a29ea2b4baeacda09359dacd155178261
Alonso <alonsete2000@gmail.com>
2022-09-29 20:00:21 +0200

    Añadida la funcion para crear la tierra

--- src/main/kotlin/Factoria.kt
+++ src/main/kotlin/Factoria.kt
@@ -1,8 +1,23 @@
+import kotlin.random.Random
+
 object Factoria {
     fun crearDron(tet:Tet){
         for(i in 0..100){
-            var d: Dron = Dron.Builder().build()
-            tet.drones.add(d)
+            var num:Int = Random.nextInt(1,2)
+            if(num == 1){
+                var d: Dron = Dron.Builder().operativo(true).build()
+                tet.drones.add(d)
+            }else if(num == 2){
+                var d: Dron = Dron.Builder().operativo(false).build()
+                tet.drones.add(d)
+            }
         }
     }
+
+    fun crearTierra():Tierra{
+        var tierra:Tierra = Tierra()
+        return tierra
+    }
+
+
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,2 +1,9 @@
 fun main() {
+    var tierra:Tierra = Factoria.crearTierra()
+    for(i in tierra.parrilla.indices){
+        for(j in tierra.parrilla[i].indices) {
+            print("${tierra.parrilla[i][j]} ")
+        }
+        println()
+    }
 }
