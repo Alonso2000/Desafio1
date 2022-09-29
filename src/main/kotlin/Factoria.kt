a29fb38abb1f3512f20796448cf195fa302abd57
Alonso <alonsete2000@gmail.com>
2022-09-29 19:32:05 +0200

    Cambio la clase factoria por un object

--- src/main/kotlin/Factoria.kt
+++ src/main/kotlin/Factoria.kt
@@ -1,4 +1,4 @@
-class Factoria {
+object Factoria {
     fun crearDron(tet:Tet){
         for(i in 0..100){
             var d: Dron = Dron.Builder().build()
--- src/main/kotlin/Tet.kt
+++ src/main/kotlin/Tet.kt
@@ -1,9 +1,6 @@
 class Tet {
     var drones = ArrayList<Dron>()
 
-    constructor(d:ArrayList<Dron>){
-        this.drones = d
-    }
     override fun toString(): String {
         return "Tet(drones=$drones)"
     }
