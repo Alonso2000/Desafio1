81617057af4031ef39c2c6e29f667a6d2f79390f
Alonso <alonsete2000@gmail.com>
2022-09-29 18:05:35 +0200

    Cambio clase orden

--- src/main/kotlin/Orden.kt
+++ src/main/kotlin/Orden.kt
@@ -3,11 +3,11 @@ import java.time.LocalDate
 class Orden {
     var fecha: LocalDate? = LocalDate.now()
     var completado:Boolean = false
-    var destino = Array(3){Array<Any>(3){0} }
-
+    var destinoFila = 0
+    var destinoColumna = 0
 
     override fun toString(): String {
-        return "Orden(fecha=$fecha, completado=$completado, destino=${destino.contentToString()})"
+        return "Orden(fecha=$fecha, completado=$completado, destinoFila=$destinoFila, destinoColumna=$destinoColumna)"
     }
 
 
