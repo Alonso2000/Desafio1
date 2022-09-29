29b55b314320f259da42cf7bf16eb2c9b4ee4c86
Alonso <alonsete2000@gmail.com>
2022-09-29 18:09:06 +0200

    Terminamos la clase reconomiciento

--- src/main/kotlin/Reconocimiento.kt
+++ src/main/kotlin/Reconocimiento.kt
@@ -1,4 +1,17 @@
-class Reconocimiento : Orden{
+import java.time.LocalDate
+
+class Reconocimiento : Orden {
     var num = 0
-    
+    var area:String = ""
+
+    constructor(f:LocalDate, c:Boolean, fil:Int, col:Int, n:Int, a:String) : super(f,c,fil,col){
+        this.num = n
+        this.area = a
+    }
+
+    override fun toString(): String {
+        return "Reconocimiento(num=$num, area='$area')"
+    }
+
+
 }
